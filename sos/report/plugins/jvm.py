9d8f54594d7316be379bc7b6640d63c837864e03
Frederic Thevenet <fthevene@redhat.com>
2026-06-17 19:21:18 +0200

    [jvm] Removed extra_cmds option
    Signed-off-by: Frederic Thevenet <fthevene@redhat.com>

--- sos/report/plugins/jvm.py
+++ sos/report/plugins/jvm.py
@@ -17,8 +17,9 @@ class Jvm(Plugin, IndependentPlugin):
     running on the system at the time of the report.
 
     This information is captured by using the 'jcmd' JDK utility with a set number
-    of predefined commands. Data collection can be extended by passing extra jcmd
-    commands as arguments.
+    of predefined commands.
+
+    If the 'clean' command is provided to
     """
 
     short_desc = 'Collect information about running Java Virtual Machines'
@@ -27,14 +28,7 @@ class Jvm(Plugin, IndependentPlugin):
     profiles = ('java',)
     commands = ('jcmd',)
 
-    # jcmd commands to run by default
-    default_cmds = {'VM.version', 'VM.info', 'System.map'}
-    # jcmd commands that should *not* run in the context of sos report
-    disallowed_cmds = {'GC.run', 'System.dump_map', 'Thread.dump_to_file', 'GC.heap_dump'}
-
     option_list = [
-        PluginOpt('extra_cmds', default='',
-                  desc='Extra jcmd commands to run for all running JVMs, separated by a space'),
         PluginOpt('jcmd_timeout', default=10,
                   desc='Timeout (in seconds) for each individual jcmd invocation')
     ]
@@ -42,7 +36,6 @@ class Jvm(Plugin, IndependentPlugin):
     def setup(self):
         jcmd_list_output = self.collect_cmd_output('jcmd -l')
         if jcmd_list_output['status'] == 0:
-            extra_cmds = set(self.get_option('extra_cmds').split())
             for jvm in jcmd_list_output['output'].splitlines():
                 proc_info = jvm.split()
                 pid = proc_info[0]
@@ -52,16 +45,13 @@ class Jvm(Plugin, IndependentPlugin):
                     # Get target process user to run jcmd as (or attaching to the VM may fail).
                     user_id = os.stat(f'/proc/{pid}').st_uid
                     user_name = pwd.getpwuid(user_id).pw_name
-                    # Get a list of commands supported by the target JVM to sanitize user input.
+                    # Get a list of commands supported by the target VM.
                     jcmd_help_output = self.exec_cmd(f'/usr/bin/jcmd {pid} help',
                                                      runas=user_name,
                                                      timeout=self.get_option('jcmd_timeout'))
                     if jcmd_help_output['status'] == 0:
                         supported_cmds = set(jcmd_help_output['output'].splitlines()[2:])
-                        for cmd in (self.default_cmds
-                            .union(extra_cmds)
-                            .difference(self.disallowed_cmds)
-                            .intersection(supported_cmds)):
+                        for cmd in {'VM.version', 'VM.info', 'System.map'}.intersection(supported_cmds):
                             self.add_cmd_output([f'/usr/bin/jcmd {pid} {cmd}'],
                                                 suggest_filename=f'{pid}_{main_class}_{cmd}',
                                                 runas=user_name,
@@ -81,6 +71,5 @@ class Jvm(Plugin, IndependentPlugin):
             r'-D([\w\d_.]*)=("([^"]*)"|\S+)',
             r'-D\1=********'
         )
-
 
 # vim: set et ts=4 sw=4 :
